e684c906b877a5d9259bdb283de68181a91e4e36
Maicon Boer <maicon_boer@hotmail.com>
2020-06-09 00:52:27 -0300

    adding features

--- src/app/controllers/HomeController.js
+++ src/app/controllers/HomeController.js
@@ -2,8 +2,6 @@ const { formatPrice, date } = require('../../lib/utils')
 
 const Product = require('../models/Product')
 
-
-
 module.exports = {
     async index(req, res) {
         let results = await Product.all()
@@ -13,8 +11,7 @@ module.exports = {
         
         async function getImage(productId) {
             let results = await Product.files(productId)
-            const files = results.rows.map(file => 
-                `${req.protocol}://${req.headers.host}${file.path.replace('public', '')}`)
+            const files = results.rows.map(file => `${req.protocol}://${req.headers.host}${file.path.replace('public', '')}`)
     
             return files[0]
         }
@@ -23,11 +20,10 @@ module.exports = {
             product.img = await getImage(product.id)
             product.oldPrice = formatPrice(product.old_price)
             product.price = formatPrice(product.price)
+            return product
         }).filter((product, index) => index > 2 ? false : true)
 
         const lastAdded = await Promise.all(productsPromise)
-
-        
 
         return res.render('home/index', { products: lastAdded })
     }
