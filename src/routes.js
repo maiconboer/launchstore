fa7a8fe959a29f3c434da5cff7f59ffc208d88bb
Maicon Boer <maicon_boer@hotmail.com>
2020-06-10 17:36:56 -0300

    adding features

--- src/routes.js
+++ src/routes.js
@@ -3,10 +3,15 @@ const routes = express.Router()
 const multer = require('./app/middlewares/multer')
 const ProductController = require('./app/controllers/ProductController')
 const HomeController = require('./app/controllers/HomeController')
+const SearchController = require('./app/controllers/SearchController')
 
-// routes 
+// home 
 routes.get('/', HomeController.index)
 
+// search
+routes.get('/products/search', SearchController.index)
+
+// products
 routes.get('/products/create', ProductController.create)
 routes.get('/products/:id', ProductController.show)
 routes.get('/products/:id/edit', ProductController.edit)
