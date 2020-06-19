036eec925916c44808f0c03808d42fa32594407f
Maicon Boer <maicon_boer@hotmail.com>
2020-06-19 12:20:07 -0300

    adding features

---
+++ src/routes/index.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const routes = express.Router()
+
+const HomeController = require('../app/controllers/HomeController')
+
+const products = require('./products')
+const users = require('./users')
+
+// home 
+routes.get('/', HomeController.index)
+
+// products
+routes.use('/products', products)
+
+// users
+routes.use('/users', users)
+
+//Alias
+routes.get('/ads/create', (req, res) => {
+    return res.redirect('/products/create')
+})
+
+routes.get('/accounts', (req, res) => {
+    return res.redirect('/users/register')
+})
+
+module.exports = routes
