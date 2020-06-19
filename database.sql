f1b2862737361c230ac1a051c1c988faf6400688
Maicon Boer <maicon_boer@hotmail.com>
2020-06-19 01:27:01 -0300

    Update code database

--- database.sql
+++ database.sql
@@ -1,3 +1,7 @@
+DROP DATABASE IF EXISTS launchstoredb;
+CREATE DATABASE launchstoredb;
+
+-- table
 CREATE TABLE "products" (
   "id" SERIAL PRIMARY KEY,
   "category_id" int NOT NULL,
@@ -12,11 +16,13 @@ CREATE TABLE "products" (
   "updated_at" timestamp DEFAULT (now())
 );
 
+-- table
 CREATE TABLE "categories" (
   "id" SERIAL PRIMARY KEY,
   "name" text NOT NULL
 );
 
+-- table
 CREATE TABLE "files" (
   "id" SERIAL PRIMARY KEY,
   "name" text,
@@ -24,6 +30,45 @@ CREATE TABLE "files" (
   "products_id" int
 );
 
+-- foreign key
 ALTER TABLE "products" ADD FOREIGN KEY ("category_id") REFERENCES "categories" ("id");
 
+-- foreign key
 ALTER TABLE "files" ADD FOREIGN KEY ("products_id") REFERENCES "products" ("id");
+
+-- table
+CREATE TABLE "users" (
+  "id" SERIAL PRIMARY KEY,
+  "name" text NOT NULL,
+  "email" text UNIQUE NOT NULL,
+  "password" text NOT NULL,
+  "cpf_cnpj" int UNIQUE NOT NULL,
+  "cep" text,
+  "address" text,
+  "created_at" timestamp DEFAULT (now()),
+  "updated_at" timestamp DEFAULT (now())
+);
+
+-- foreign key
+ALTER TABLE "products" ADD FOREIGN KEY ("user_id") REFERENCES "users" ("id");
+
+-- create procedure
+CREATE FUNCTION trigger_set_timestamp()
+RETURNS TRIGGER AS $$
+BEGIN
+  NEW.updated_at = NOW();
+  RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+-- auto updated_at products
+CREATE TRIGGER set_timestamp
+BEFORE UPDATE ON products
+FOR EACH ROW
+EXECUTE PROCEDURE trigger_set_timestamp();
+
+-- auto updated_at users
+CREATE TRIGGER set_timestamp
+BEFORE UPDATE ON users
+FOR EACH ROW
+EXECUTE PROCEDURE trigger_set_timestamp();
